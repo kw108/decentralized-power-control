b0933b3c5e9c78b6165990c2d199d0d64ac954fe
Kaiser Wang <53930614+kw108@users.noreply.github.com>
2022-11-08 17:30:33 +0800

    Update synthesize-channel-gains.py

--- synthesize-channel-gains.py
+++ synthesize-channel-gains.py
@@ -14,10 +14,12 @@ class RandGenerator:
         num_aps: number of access points
         filename: channel gain datasets in .mat file
     """
-    def __init__(self, num_ues, num_aps, filename):
+    def __init__(self, num_ues, num_aps, filename_gain, filename_rate, filename_locs):
         self.num_ues = num_ues
         self.num_aps = num_aps
-        self.channel_gain = scipy.io.loadmat(filename)
+        self.channel_gain = scipy.io.loadmat(filename_gain)
+        self.ue_rates = scipy.io.loadmat(filename_rate)
+        self.locs = scipy.io.loadmat(filename_locs)
 
     """
         The generate function generates channel gain datasets under given fading types with path loss. It reshape and 
@@ -29,15 +31,15 @@ class RandGenerator:
     """
     def generate(self, num_samples, fading_type, offset=0):
         # generate locations and compute distances from ues to aps
-        ue_locs = np.random.uniform(low=0, high=1, size=(self.num_ues, 2))
-        ap_locs = np.random.uniform(low=0, high=1, size=(self.num_aps, 2))
-        dist = np.zeros((self.num_ues, self.num_aps))
-        for ue in range(self.num_ues):
-            for ap in range(self.num_aps):
-                dist[ue, ap] = np.maximum(np.linalg.norm(ue_locs[ue] - ap_locs[ap], ord=1), 0.035)
-
-        # compute path loss according to distances. The formula requires d>=0.035 to hold. Omit factor 10 ** (-12.8).
-        path_loss = dist ** (-3.76)
+        ap_locs = np.abs(self.locs['ap_locs'])
+        ue_locs = np.abs(self.locs['ue_locs'])
+        try:
+            assert num_samples * self.num_aps * 2 + offset < len(ap_locs)
+            assert num_samples * self.num_ues * 2 + offset < len(ue_locs)
+            ap_locs = ap_locs[offset: num_samples * self.num_aps * 2 + offset].reshape((num_samples, self.num_aps, 2))
+            ue_locs = ue_locs[offset: num_samples * self.num_ues * 2 + offset].reshape((num_samples, self.num_ues, 2))
+        except AssertionError:
+            print('Trying to generate more samples than allowed.')
 
         # generate service times and map them to compute required ue rates
         serve_times = np.random.uniform(low=0, high=24, size=(num_samples, self.num_ues))
@@ -54,21 +56,43 @@ class RandGenerator:
             gain = np.random.choice(np.ravel(gain), size=(num_samples, self.num_aps, self.num_ues, self.num_ues),
                                     replace=True)
 
-        # compute G matrix. Since 20dB - 25dB SINR value is typically recommended for data networks, we could scale the
-        # diagonal entries in G by approximately 10 ** (22.5 / 20) * num_ues to compensate for extra techniques like
-        # antenna gain and coding gain, etc.
+        rate = np.abs(self.ue_rates['ue_rates'])
+        try:
+            assert num_samples * self.num_ues + offset < len(rate)
+            rate = rate[offset: num_samples * self.num_ues + offset].reshape((num_samples, self.num_ues))
+        except AssertionError:
+            print(np.shape(rate))
+            rate = np.random.choice(np.ravel(rate), size=(num_samples, self.num_ues), replace=True)
+
+        # compute G matrix. Since an approximate 25dB SINR value is recommended for medium- to high-quality data
+        # networks, we could scale the diagonal entries in G by approximately 10 ** (22.5 / 10) * num_ues to compensate
+        # for extra techniques like antenna gain and coding gain, etc.
+        dist = np.zeros((self.num_ues, self.num_aps))
         for i in range(num_samples):
+            for ue in range(self.num_ues):
+                for ap in range(self.num_aps):
+                    dist[ue, ap] = np.maximum(np.linalg.norm(ue_locs[i, ue] - ap_locs[i, ap], ord=1), 0.035)
+            # compute path loss according to distances. The formula requires d>=0.035 to hold.
+            path_loss = dist ** (-3.76)
             for j in range(self.num_aps):
                 gain[i, j, :, :] = np.matmul(np.diag(path_loss[:, j]) ** 0.5, gain[i, j, :, :])
                 gain[i, j, :, :] = np.matmul(gain[i, j, :, :], np.diag(path_loss[:, j]) ** 0.5)
-                gain[i, j, :, :] += np.eye(self.num_ues) * gain[i, j, :, :] * 10 ** (22.5 / 20) * (self.num_ues - 1)
-        return [gain, ue_rates]
+                gain[i, j, :, :] += np.eye(self.num_ues) * gain[i, j, :, :] * 10 ** (25 / 10) * self.num_ues
+        return [gain * 10 ** (-12.8), rate]
 
 
 if __name__ == '__main__':
-    rand_generator = RandGenerator(2, 4, 'channel_gain.mat')
-    gain, ue_rates = rand_generator.generate(10, 'rayleigh_gain', offset=0)
-    with open('channel_gain.npy', 'wb') as file:
-        pickle.dump({'gain': gain, 'ue_rates': ue_rates}, file)
-    with open('channel_gain.npy', 'rb') as file:
-        data = pickle.load(file)
+    # set global parameters
+    num_aps = 4
+    num_ues = 2
+    noise_dbm = -60
+    eps = 1.0e-06
+    filename_gain = 'channel_gain.mat'
+    filename_rate = 'ue_rates.mat'
+    filename_locs = 'locs.mat'
+    fading_type = 'weibull_gain'
+    num_samples = 1000
+    num_training = 900
+    num_test = 100
+    rand_generator = RandGenerator(num_ues, num_aps, filename_gain, filename_rate, filename_locs)
+    gains, ue_rates = rand_generator.generate(num_samples, fading_type, offset=0)
